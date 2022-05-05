b279f9cd05bd3d37383ca4b01d8c4fd7010ae0ca
Hansana1999 <chaminduhansana5@gmail.com>
2022-05-05 16:04:18 +0530

    Front end

---
+++ src/data.js
@@ -0,0 +1,92 @@
+export const sliderItems = [
+   {
+       id: 1,
+       img: "https://previews.123rf.com/images/imagedb/imagedb1108/imagedb110805246/10168281-portrait-of-a-man-carrying-shopping-bags.jpg",
+       title: "SUMMER SALE",
+       desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS." ,
+       bg: "f5fafd",
+   },
+   {
+    id: 2,
+    img: "https://previews.123rf.com/images/dolgachov/dolgachov1211/dolgachov121100920/16444109-picture-of-handsome-man-in-suit-with-shopping-bags.jpg",
+    title: "WINTER SALE",
+    desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS." ,
+    bg: "f5fafd",
+},
+{
+    id: 3,
+    img: "https://thumbs.dreamstime.com/b/sacs-provisions-heureux-de-prise-du-consommateur-d-homme-achat-et-vente-lois-protection-des-consommateurs-assurer-droites-150849790.jpg",
+    title: "AUTUMN COLLECTION",
+    desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS." ,
+    bg: "f5fafd",
+},
+
+
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "./img/casual.jpg",
+        title: "CASHUAL STYLE",
+    },
+    {
+        id: 2,
+        img: "./img/denim.jpg",
+        title: "DENIM FASION",
+    },
+     {
+        id: 3,
+        img: "./img/formal.jpg",
+        title: "FORMAL STYLE",
+    }
+
+
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://i.ebayimg.com/images/g/iJ0AAOSw6AdfULXj/s-l400.jpg",
+      
+    },
+    {
+        id: 2,
+        img: "https://static-01.daraz.lk/p/662a67563dd34e90e260d3e9e73dde52.jpg",
+        
+    },
+     {
+        id: 3,
+        img: "https://4.imimg.com/data4/LE/OD/MY-36614984/dsc_1232-500x500.jpg",
+        
+    },
+    {
+        id: 4,
+        img: "https://res.cloudinary.com/webstore/image/fetch/w_500,c_limit,c_fit,f_auto,e_trim/https%3A%2F%2Fae01.alicdn.com%2Fkf%2FHe678af0fd980480da7e2580895df3b25W%2FMen-Casual-Shirt-Slim-Men-Casual-Striped-Shirt-Long-Sleeve-Formal-Dress-Shirts-Men-Male-Clothing.jpg",
+      
+    },
+    {
+        id: 5,
+        img: "https://5.imimg.com/data5/MR/BM/UF/SELLER-98062481/mens-faded-denim-jeans-500x500.png",
+        
+    },
+     {
+        id: 6,
+        img: "https://3.imimg.com/data3/NH/VW/GLADMIN-31957/men-hat-500x500.jpg",
+        
+    },
+    {
+        id: 7,
+        img: "https://image.made-in-china.com/202f0j00NosgcIbGrrkd/Hot-Selling-Shorts-Men-Training-Gym-Shorts-Your-Own-Design-High-Quality-Gym-Shorts-Pants-for-Men.jpg",
+      
+    },
+    {
+        id: 8,
+        img: "https://m.media-amazon.com/images/I/71T8HkuK+8L._UL1500_.jpg",
+        
+    },
+     
+
+
+]
+
